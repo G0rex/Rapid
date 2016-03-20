5d07137435430fa4d2e432ad6e10a802cafb18d6
root <root@localhost.localdomain>
2016-03-20 12:49:06 +0200

    Task_5

---
+++ lesson5/5.js
@@ -0,0 +1,42 @@
+"use strict"
+const http = require("http");
+
+var fs = require('fs');
+var Array;
+var ListOfRegResult;
+var counter = 0;
+var resultOfTask = [];
+fs.readFile('./addresses.js', 'utf8', function (err, contents) {
+    var t = /'.+?'/g;//парсит по скобкам все строки
+
+
+    while ((Array = t.exec(contents)) != null) {
+
+        //var tt = /(\s?[улпрет.-]{2,5}\s)/;//парсит переулки и тд
+        //var tt = /(\s?[улпрет.-]{2,5}\s)?\s?(([0-9-]{2,5})?[а-яА-Я.\s]+),?\s?/;//парсит улицу норм)
+        //норм парсит дома
+        //var tt = /(\s?[улпрет.-]{2,5}\s)?\s?(([0-9-]{2,5})?[а-яА-Я.\s]+),?\s([дом]{3,3})?\s?([0-9А-Я-]+)?\/?([\d]+)?/;
+        var resultOfReg = /(\s?[улпрет.-]{2,5}\s)?\s?(([0-9-]{2,5})?[а-яА-Я.\s]+),?\s?([дом]{3,3})?((\s)+)?([0-9А-Я-]+)?\/?,?\s?([а-я.]+\s)?([\d]+)?/;
+
+        ListOfRegResult = resultOfReg.exec(Array[0]);
+
+        var Obj = {};
+        if (ListOfRegResult != null) {
+            Obj.street = ListOfRegResult[2];
+            if (ListOfRegResult[7] != null)
+                Obj.house = ListOfRegResult[7];
+            if (ListOfRegResult[9] != null)
+                Obj.flat = ListOfRegResult[9];
+        }
+
+        resultOfTask [counter] = Obj;
+
+        counter++;
+    }
+    console.log(resultOfTask);
+});
+
+console.log('after calling readFile');
+
+let server = http.createServer();
+server.listen(3001);
