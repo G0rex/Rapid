6a1b5e4457d227bb2a629562cf4b3e9977dd9c3e
Igor <Morgunov.Igor1990@gmail.com>
2016-03-25 21:37:40 +0200

    Task_6

--- lesson6/app/controllers/MemcachedController.js
+++ lesson6/app/controllers/MemcachedController.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /**
- curl -v -X DELETE "http://127.0.0.1:8081/memcached" -d '{"key":"bar"}' -H "Content-Type: application/json"
+    curl -v -X DELETE "http://127.0.0.1:8081/memcached" -d '{"key":"bar"}' -H "Content-Type: application/json"
  curl -v -X PUT "http://127.0.0.1:8081/memcached" -d '{"key":"bar","value":"valera","expires":360}' -H "Content-Type: application/json"
  curl -v -X GET "http://127.0.0.1:8081/memcached/bar"
  curl -v -X POST "http://127.0.0.1:8081/memcached" -d '{"key":"bar","value":"foo","expires":360}' -H "Content-Type: application/json"
@@ -9,71 +9,64 @@
  */
 const co = require('co');
 let Memcached = require('co-memcached');
+//let Memcached = require('memcached');
 let client = new Memcached("127.0.0.1:11211");
 
 
 module.exports = {
 
     getAction: function * (next) {
-try {
-        this.body=yield client.get(this.params.key);
+        try {
+            var result=yield client.get(this.params.key);
+            this.body=result;
 
+        } catch (e) {
+            this.status = 400;
+            this.body = {message: "Bad Request"};
+        }
         yield next;
-  } catch (e) {
-                this.status = 400;
-                this.body = {message: "Bad Request"};
-            }
     },
     putAction: function * (next) {
+        console.log("PUT");
 
 
-        var coWrapper = co.wrap(function*() {
             try {
-                this.body=this.request.body;
-                client.replace(this.request.body.key, this.request.body.value, this.request.body.expires);
 
+                yield client.replace(this.request.body.key, this.request.body.value, this.request.body.expires);
+                this.status=201;
             } catch (e) {
                 this.status = 400;
                 this.body = {message: "Bad Request"};
             }
+            yield next;
 
-        }.bind(this));
-        coWrapper();
     },
 
     postAction: function * (next) {
-        var coWrapper = co.wrap(function*() {
 
-            try {
- 
-                this.body=this.request.body;
-                 client.set(this.request.body.key, this.request.body.value, this.request.body.expires);
-                console.log(client.get(this.request.body.key));
-
-            } catch (e) {
-                this.status = 400;
-                this.body = {message: "Bad Request"};
-            }
-
-        }.bind(this));
-        coWrapper();
+                //console.log(this.request.body.key+"3333333");
+                //  this.body=this.request.body;
+        try {
+                yield client.set(this.request.body.key, this.request.body.value, this.request.body.expires);
+                this.status = 201;
+        } catch (e) {
+            this.status = 400;
+            this.body = {message: "Bad Request"};
+        }
+                yield next;
+                //console.log(client.get(this.request.body.key));
 
     },
     deleteAction: function * (next) {
 
-        var coWrapper = co.wrap(function*() {
-            try {
 
-                this.body=this.request.body;
-                client.del(this.request.body.key);
-                console.log(client.get(this.request.body.key));
+            try {
+                yield client.del(this.request.body.key);
+                this.status=200;
             } catch (e) {
                 this.status = 400;
                 this.body = {message: "Bad Request"};
             }
-
-
-        }.bind(this));
-        coWrapper();
+        yield next;
     }
 };
